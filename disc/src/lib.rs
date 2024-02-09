ce7631c97f7d91c0c9a4b705822babc363835143
ryan kurte <ryan@kurte.nz>
2024-02-10 12:24:47 +1300

    testing alternate mux for discovery sim

--- disc/src/lib.rs
+++ disc/src/lib.rs
@@ -1,5 +1,5 @@
 
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::{Arc, Mutex}, fmt::Debug};
 
 use async_trait::async_trait;
 use clap::Parser;
@@ -123,6 +123,101 @@ impl NetMux {
     }
 }
 
+type PeerTable = HashMap<Id, PeerHandle>;
+
+type PeerHandle = UnboundedSender<PeerMessage>;
+
+type PeerMessage = (
+    Entry<Id, Info>,
+    Request<Id, Data>,
+    OneshotSender<Response<Id, Info, Data>>,
+);
+
+pub struct NetMux2 {
+    peers: Arc<Mutex<PeerTable>>,
+}
+
+impl NetMux2 {
+    pub fn new() -> Self {
+        Self{ peers: Arc::new(Mutex::new(HashMap::new()))}
+    }
+
+    pub fn handle(&self, id: &Id, handle: PeerHandle) -> NetMuxHandle2 {
+        {
+            let mut p = self.peers.lock().unwrap();
+            p.insert(id.clone(), handle);
+        }
+
+        NetMuxHandle2 {
+            id: id.clone(),
+            peers: self.peers.clone(),
+        }
+    }
+
+    pub fn exit(self) {
+
+    }
+}
+
+#[derive(Clone)]
+pub struct NetMuxHandle2 {
+    id: Id,
+    peers: Arc<Mutex<PeerTable>>,
+}
+
+impl core::fmt::Debug for NetMuxHandle2 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("NetMuxHandle2")
+            .field("id", &self.id)
+            .finish()
+    }
+}
+
+#[async_trait]
+impl Net<Id, Info, Data> for NetMuxHandle2 {
+    async fn request(
+        &self,
+        peers: Vec<Entry<Id, Info>>,
+        req: Request<Id, Data>,
+    ) -> Result<HashMap<Id, Response<Id, Info, Data>>, Error> {
+        debug!("Request: {req:?} to peers: {peers:?}");
+
+        // Grab handles for each target
+        let handles: Vec<_> = {
+            let p = self.peers.lock().unwrap();
+            let h = peers.iter().filter_map(|e| p.get(e.id()).map(|h| (e.clone(), h.clone())) ).collect();
+            drop(p);
+            h
+        };
+
+        // Issue request to each peer
+        let mut resps = vec![];
+        for (e, handle) in handles {
+            let target_id = e.id().clone();
+
+            // Send request via mux
+            let (resp_tx, resp_rx) = oneshot::channel();
+            handle.send((
+                    Entry::new(self.id.clone(), ()),
+                    req.clone(),
+                    resp_tx,
+                ))
+                .unwrap();
+
+            // Collect response futures
+            resps.push(async move {
+                resp_rx.await.map(|v| (target_id, v) )
+            });
+        }
+
+        // Await response futures
+        let mut resps = join_all(resps).await;
+
+        // Return response collection
+        Ok(HashMap::from_iter(resps.drain(..).filter_map(|r| r.ok() )))
+    }
+}
+
 #[derive(Clone)]
 pub struct NetMuxHandle {
     id: Id,
@@ -176,23 +271,19 @@ impl Net<Id, Info, Data> for NetMuxHandle {
 }
 
 
-pub struct MockPeer {
+pub struct MockPeer<H: Net<Id, Info, Data> + Clone + Debug + 'static> {
     pub dht_handle: DhtHandle<Id, Info, Data>,
-    task_handle: JoinHandle<Dht<Id, (), u32, NetMuxHandle>>,
+    task_handle: JoinHandle<Dht<Id, (), u32, H>>,
     exit_tx: OneshotSender<()>,
 }
 
 
-impl MockPeer {
+impl <H: Net<Id, Info, Data> + Clone + Debug> MockPeer<H> {
     pub fn new(
         id: Id,
         config: DhtConfig,
-        mut rx: UnboundedReceiver<(
-            Entry<Id, Info>,
-            Request<Id, Data>,
-            OneshotSender<Response<Id, Info, Data>>,
-        )>,
-        tx: NetMuxHandle,
+        mut rx: UnboundedReceiver<PeerMessage>,
+        tx: H,
     ) -> Self {
         // Setup exit handler
         let (exit_tx, exit_rx) = oneshot::channel();
@@ -237,7 +328,7 @@ impl MockPeer {
         }
     }
 
-    pub async fn exit(self) -> Dht<Id, (), u32, NetMuxHandle> {
+    pub async fn exit(self) -> Dht<Id, (), u32, H> {
         let _ = self.exit_tx.send(());
         self.task_handle.await.unwrap()
     }
